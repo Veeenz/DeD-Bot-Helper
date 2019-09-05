4f4d8dd3410dd4b128e27266d458e475b57d2221
Vincenzo Filetti <vzfiletti@gmail.com>
2019-09-05 12:05:45 +0200

    First commit

---
+++ main.ts
@@ -0,0 +1,35 @@
+
+import Discord, { Message } from 'discord.js'
+import * as config from './config.json';
+
+const client = new Discord.Client();
+const types = ['d4', 'd6', 'd8', 'd10', 'd12', 'd20', 'd100'];
+
+const rollDice = (type: string, numberOfDice=1): Array<number> => {
+    const face: number = parseInt(type.replace("d",""));
+    let dices: Array<number> = [];
+    for(let i = 0; i < numberOfDice; i++) {
+        dices.push(Math.floor((Math.random()*face)+1))
+    }
+    return dices
+}
+
+client.on('message', (message: Message) => {
+    if(!message.content.startsWith(config.prefix)) return
+    if (message.content.startsWith(`${config.prefix}roll`)) {        
+        const args = message.content.split(" ");
+        
+        if(args.length <= 1) {
+            return message.reply("Incorrect syntax, send me something like !roll d4 or !roll d20");
+        } else {
+            let dado = args[1]
+            if(types.includes(dado)) {
+                message.reply(rollDice(dado));
+            } else {
+                message.reply("Not a valid dice!");
+            }
+        }
+    }
+})
+
+client.login(config.token);
